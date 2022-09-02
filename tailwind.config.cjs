078c7d1c66bfb6da6c639a67c03419e105a85ca2
Manan Joshi <mananjoshi1995@gmail.com>
2022-09-01 23:12:42 -0500

    feat: extending tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: colors.gray['700'],
+          light: colors.gray['50']
+        }
+      }
+    }
   },
   plugins: []
 };
