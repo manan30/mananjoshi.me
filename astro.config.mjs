c6fd84e29351422799682a7007f3b8e5e32b41b1
Manan Joshi <mananjoshi1995@gmail.com>
2023-04-01 13:19:40 -0500

    chore: updating dev server port

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown(), react()]
+  integrations: [tailwind(), partytown(), react()],
+  server: { port: 3002 }
 });
